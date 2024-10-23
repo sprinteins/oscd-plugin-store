7c98b3debac0925de6e1060013bfd9fd8e599a2f
Rasul Adasov <rasul@futureblur.com>
2024-10-23 11:07:29 +0200

    refactor: simplify sort

--- src/plugin-store.svelte
+++ src/plugin-store.svelte
@@ -121,9 +121,7 @@ function combineAllPlugins(local: Plugin[], external: Plugin[]): Plugin[] {
 	}
 
 	plugins.sort((a, b) => {
-		if (a.name < b.name) return -1;
-		if (a.name > b.name) return 1;
-		return 0;
+		return a.name.localeCompare(b.name);
 	});
 
 	return plugins;
