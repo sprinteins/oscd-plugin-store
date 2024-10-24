5b2bdb7521de59b28384c48334a70ba3c91df2f6
Rasul Adasov <rasul@futureblur.com>
2024-10-24 15:21:07 +0200

    feat: restrict store functionality with flag

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,44 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import fs from "fs/promises";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        svelte(),
-        cssInjectedByJsPlugin({
-            styleId: process.env.npm_package_name,
-            injectCodeFunction: function injectCodeCustomRunTimeFunction(
-                cssCode: string, options: any) {
-                if (!globalThis.pluginStyle) {
-                    globalThis.pluginStyle = {}
-                }
-                globalThis.pluginStyle[options.styleId] = cssCode
-            }
-        }),
-    ],
-    server: {
-        port: 54187,
-    },
-    build: {
-        lib: {
-            entry: 'src/plugin.ts', 
-            fileName: process.ENV.VITE_EXTERNAL_PLUGINS === "true" ? "index.js" : "index-restricted.js",
-            formats: ['es'],
-        },
-    },
-})
+	plugins: [
+		svelte(),
+		cssInjectedByJsPlugin({
+			styleId: process.env.npm_package_name,
+			injectCodeFunction: function injectCodeCustomRunTimeFunction(
+				cssCode: string,
+				options: any,
+			) {
+				if (!globalThis.pluginStyle) {
+					globalThis.pluginStyle = {};
+				}
+				globalThis.pluginStyle[options.styleId] = cssCode;
+			},
+		}),
+		{
+			name: "index-html-config",
+			async transformIndexHtml() {
+				if (process.env.VITE_EXTERNAL_PLUGINS !== "true") {
+					return await fs.readFile("index-restricted.html", "utf-8");
+				}
+			},
+		},
+	],
+	server: {
+		port: 54187,
+	},
+	build: {
+		lib: {
+			entry: "src/plugin.ts",
+			fileName:
+				process.env.VITE_EXTERNAL_PLUGINS === "true"
+					? "index"
+					: "index-restricted",
+			formats: ["es"],
+		},
+	},
+});
